f96de429cd11400aeec63e9de2c0f0d6aad332d4
thedantonn <138753839+thedantonn@users.noreply.github.com>
2025-01-14 00:20:09 +0530

    Updated firebase config and header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -73,14 +73,14 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link to={"/browse"} className="-m-1.5 p-1.5">
               <span className="sr-only">Article Hub</span>
               <img
-                alt="logo"
-                src="https://www.pngfind.com/pngs/m/564-5646320_hd-png-psd-free-download-ah-logo-graphic.png0"
+                alt="Article Hub"
+                src="https://www.pngfind.com/pngs/m/564-5646320_hd-png-psd-free-download-ah-logo-graphic.png"
                 className="h-8 w-auto"
               />
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -92,20 +92,13 @@ export default function Header() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                    Product 
-                  </DisclosureButton>
-                </Disclosure>
-              </div>
-              <div className="py-6">
+              {!user ? (<div className="py-6">
               <Link to={"/login"}
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Log in
                 </Link>
-              </div>
+              </div>):(<div className="lg:flex lg:flex-1 lg:justify-end"><button onClick={handleSignout} className='text-sm/6 font-semibold text-gray-900'>Logout</button></div>)}
             </div>
           </div>
         </DialogPanel>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCBRtvin8m4785mQmBjYg71GfyAbikV8qY",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "articlehub-4999d.firebaseapp.com",
   projectId: "articlehub-4999d",
   storageBucket: "articlehub-4999d.firebasestorage.app",
